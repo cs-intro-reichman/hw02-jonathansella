16e287183bf18b3bd2669a9bc05499e6b6dd1cad
unknown <jonathansella1@gmail.com>
2024-01-03 13:48:48 +0200

    homework 2

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,8 +3,16 @@ class DamkaBoard {
 		int a = Integer.parseInt(args[0]);
 		int b = a - 1;
 		String s = "";
-    while (a > 0) {a = a - 1; s = s + "* ";}
-	while (b >= 0) {System.out.println(s); b = b - 1; if (b % 2 == 0) {s = " " + s;}
-	else {s = s.substring(1, s.length());}}	
+    while (a > 0) {
+		a = a - 1; s = s + "* ";
+	}
+	while (b >= 0) {
+		System.out.println(s); b = b - 1;
+		 if (b % 2 == 0) {
+			s = s.substring(1, s.length());
+		} else {
+			s = " " + s;
+		}
+	}	
 	}
 	}
