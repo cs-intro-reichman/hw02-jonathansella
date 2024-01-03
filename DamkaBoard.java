29f2fde105dbfa746d6c23ff4745d6515cdf1bf2
unknown <jonathansella1@gmail.com>
2024-01-03 14:22:43 +0200

    o

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -10,9 +10,9 @@ class DamkaBoard {
 		}
 	    for (int i = 0; i < a; i++) {
 			if (i % 2 == 0) {
-				System.out.println(s);
+				System.out.println(s + " ");
 			} else {
-				System.out.println( " " + s);
+				System.out.println(" " + s);
 			}
 		}
 		System.out.println();
